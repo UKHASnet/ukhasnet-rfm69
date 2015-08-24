a0c33f548577d4aae739b28ff483c51213fc3a4a
Jon Sowman <jon@hexoc.com>
2015-08-24 12:03:39 +0100

    fixed a typo

--- ukhasnet-rfm69.h
+++ ukhasnet-rfm69.h
@@ -790,7 +790,7 @@ rfm_status_t rf69_sampleRssi(int16_t* rssi);
 rfm_status_t spi_init(void);
 rfm_status_t spi_exchange_single(const rfm_reg_t out, rfm_reg_t* in);
 rfm_status_t spi_ss_assert(void);
-rfm_staus_t spi_ss_deassert(void);
+rfm_status_t spi_ss_deassert(void);
 
 #endif /* __RFM69_H__ */
 
